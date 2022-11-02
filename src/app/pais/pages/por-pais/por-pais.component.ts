fef4175aca3c65f6bb095ca6d83658a160e3c8da
Daniel <daniel.morocho@simetrik.com>
2022-11-02 08:19:21 -0500

    Routing: How to use routing in angular - practice

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+})
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
+  constructor(private paisService: PaisService) {}
+
+  buscar(termino: string) {
+    this.termino = termino;
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      },
+      (err) => {
+        this.hayError = true;
+        console.log('Error');
+        console.info(err);
+        this.paises = [];
+      }
+    );
+  }
+}
